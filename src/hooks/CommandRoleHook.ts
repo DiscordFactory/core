660dcc659925bf471d3e881259fd545a09a6ebc0
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-05-10 01:20:15 +0200

    :construction: Add CommandRoleHook

---
+++ src/hooks/CommandRoleHook.ts
@@ -0,0 +1,26 @@
+import { GuildMember } from 'discord.js'
+import Hook from '../decorators/Hook'
+import BaseHook from '../interface/HookInterface'
+import CommandContext from '../contexts/CommandContext'
+
+@Hook('app:command:preload')
+export default class CommandRoleHook implements BaseHook {
+  public async run(context: CommandContext): Promise<any> {
+    const { sender, command, message } = context
+    if (command.roles) {
+      const hasRole = (sender: GuildMember | null, roles: Array<string>) => {
+        if (!sender) {
+          return false
+        }
+        return roles.some((role: string) => {
+          return sender.roles.cache.has(role)
+        })
+      }
+
+      if (!hasRole(sender, command.roles)) {
+        context.setCancelled(true)
+        return await message.reply('You are not allowed to execute this command.')
+      }
+    }
+  }
+}
