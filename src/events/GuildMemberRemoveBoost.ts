2bc4236ea8a6517853575546d80a978f1cfc3262
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-09-23 21:47:36 +0200

    :sparkles: Implement GuildMemberAddBoost and GuildMemberRemoveBoost

---
+++ src/events/GuildMemberRemoveBoost.ts
@@ -0,0 +1,21 @@
+import Factory from '../Factory'
+
+export default class GuildMemberRemoveBoost {
+  constructor (public factory: Factory) {
+  }
+
+  public async handle () {
+    this.factory.client?.ws.on('GUILD_MEMBER_UPDATE', (payload) => {
+      const guild = this.factory.client?.guilds.cache.get(payload.guild_id)
+      const member = guild?.members.cache.get(payload.user.id)
+
+      if (!payload.premium_since) {
+        return
+      }
+
+      if (member?.premiumSince! && payload.premium_since) {
+        this.factory.client?.emit('guildMemberRemoveBoost', member)
+      }
+    })
+  }
+}
