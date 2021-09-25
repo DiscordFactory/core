3d73d95f8c1bcabf87c1d224a3895814408b5c95
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-09-26 01:03:31 +0200

    :sparkles: feat : Add ContextMenuEntity

--- src/Container.ts
+++ src/Container.ts
@@ -2,10 +2,12 @@ import { ClientEvents, Collection } from 'discord.js'
 import { EventEntity } from './entities/Event'
 import { CommandEntity } from './entities/Command'
 import { ProviderEntity } from './entities/Provider'
+import { ContextMenuEntity } from './entities/ContextMenu'
 
 export default class Container {
   public events: EventEntity<keyof ClientEvents>[] = []
   public commands: CommandEntity[] = []
+  public contextMenu: ContextMenuEntity[] = []
   public providers: ProviderEntity[] = []
   public cli: Collection<string, any> = new Collection()
 }
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import NodeEmitter from './utils/NodeEmitter'
 import { AddonCommand, BaseAddonCommand, BaseAddon, CLICommand, BaseAddonHook, BaseAddonEvent } from './entities/Addon'
 import { Event, BaseEvent, EventEntity } from './entities/Event'
 import { Command, BaseCommand, CommandEntity } from './entities/Command'
+import { ContextMenu, BaseContextMenu, ContextMenuEntity } from './entities/ContextMenu'
 import { Hook, BaseHook, HookEntity } from './entities/Hook'
 import { CommandContainer, EventContainer, HookContainer, ProviderContainer, EntityResolvable, AddonContext } from './types'
 import { BaseProvider, ProviderEntity } from './entities/Provider'
@@ -26,6 +27,9 @@ export {
   Command,
   BaseCommand,
 
+  ContextMenu,
+  BaseContextMenu,
+
   Hook,
   BaseHook,
 
@@ -39,6 +43,7 @@ export {
   EntityResolvable,
   EventEntity,
   CommandEntity,
+  ContextMenuEntity,
   HookEntity,
   ProviderEntity,
 
